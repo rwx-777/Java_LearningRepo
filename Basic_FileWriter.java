b98ce3c1a17a47d98a450234b7f8f809bea65b61
rwx777 <joshuajared007@gmail.com>
2019-11-06 23:03:27 +0100

    Add files via upload

---
+++ Basic_FileWriter.java
@@ -0,0 +1,23 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Basic_FileWriter {
+
+	public static void main(String[] args) {
+		
+		String Text_to_Write = "Hello there";
+		
+		try(PrintWriter out = new PrintWriter("/home/elliot/data.txt") ){ //Path to File to be stored
+			
+			out.println(Text_to_Write); //Text gets printed out into the File 
+			
+		}catch (FileNotFoundException e) {
+			e.printStackTrace(); //Print out Errors if something goes wrong
+		}
+		
+		//if you see this then everything is fine
+		System.out.println("All Done Sir");
+
+	}
+
+}
